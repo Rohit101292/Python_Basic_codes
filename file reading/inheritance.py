f47d399dee88d4242dbcb249eb2a05edd7c804f4
Rohit Dadala <rohitdadala@gmail.com>
2021-12-22 18:36:30 +1000

    try

---
+++ file reading/inheritance.py
@@ -0,0 +1,17 @@
+from new.py import student
+from new.py import random
+
+
+class person(student):
+    pass
+
+
+
+class Rohit(random):
+    def Ro(rohit):
+
+
+p1= person()
+
+print(p1.name)
+print(p1.age)
